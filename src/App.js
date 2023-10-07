4d3f47765009a4cc653cdd0863aa63903139ef62
minhphamhk123 <minhphamhklovetory@gmail.com>
2023-10-07 20:57:23 +0700

    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, Update using tinyMCE api

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import './App.css';
+import React from 'react';
+import { Editor } from '@tinymce/tinymce-react';
 
-function App() {
-  return (
-  <div className="App">
-    <textarea>
-      Welcome to TinyMCE!
-    </textarea>
-    <div>
-      <script>
-        
-      </script>
-    </div>
-  </div>
-  );
+class App extends React.Component {
+  handleEditorChange = (e) => {
+    console.log('Content was updated:', e.target.getContent());
+  }
+
+  render() {
+    return (
+      <Editor
+        initialValue="<p>This is the initial content of the editor</p>"
+        init={{
+          plugins: 'link image code ai tinycomments',
+          toolbar: 'undo redo | bold italic | alignleft aligncenter alignright | code | tinycomments |',
+          ai_request: (request, respondWith) => respondWith.string(() => Promise.reject("See docs to implement AI Assistant"))
+        }}
+        onChange={this.handleEditorChange}
+      />
+    );
+  }
 }
 
 export default App;
