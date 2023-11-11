0cfce3c26fbc4aa3c6d5726d576b54ffe524fe79
minhphamhk123 <minhphamhklovetory@gmail.com>
2023-11-12 02:12:19 +0700

    connected to db

--- api/index.js
+++ api/index.js
@@ -1,5 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+mongoose
+  .connect(process.env.Mongo)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+  
 const app = express();
 
 app.listen(3000, () => {
