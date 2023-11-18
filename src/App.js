c8937ad05983854b027e2f700d0fdf5ca76eebba
thaoht194 <thaohtgm@gmail.com>
2023-11-18 23:49:46 +0700

    commit temp

--- src/App.js
+++ src/App.js
@@ -7,6 +7,19 @@ class App extends React.Component {
     console.log('Content was updated:', e.target.getContent());
   }
 
+  // render() {
+  //   return (
+  //     <Editor
+  //       initialValue="<p>This is the initial content of the editor</p>"
+  //       init={{
+  //         plugins: 'link image code ai tinycomments',
+  //         toolbar: 'undo redo | bold italic | alignleft aligncenter alignright | code | tinycomments |',
+  //         ai_request: (request, respondWith) => respondWith.string(() => Promise.reject("See docs to implement AI Assistant"))
+  //       }}
+  //       onChange={this.handleEditorChange}
+  //     />
+  //   );
+  // }
   render() {
     return (
       <Editor
