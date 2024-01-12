86b7b35bdd848cf40cdcba9177f14535ca8a9c3c
minhphamhk123 <minhphamhklovetory@gmail.com>
2024-01-12 17:50:12 +0700

    Small update

--- client/src/pages/auth/SignIn.jsx
+++ client/src/pages/auth/SignIn.jsx
@@ -1,18 +1,18 @@
-import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import {
   signInStart,
   signInSuccess,
   signInFailure,
-} from "../../redux/user/userSlice";
-import Background from "../../components/common/auth/Background";
-import { useDispatch, useSelector } from "react-redux";
-import OAuth from "../../components/OAuth";
+} from '../../redux/user/userSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import OAuth from '../../components/OAuth';
+import styles from '../../styles/SignIn.module.css'
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
   const { loading, error } = useSelector((state) => state.user);
-  const [showError, setShowError] = useState(false);
+
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const handleChange = (e) => {
@@ -23,69 +23,60 @@ export default function SignIn() {
     e.preventDefault();
     try {
       dispatch(signInStart());
-      const res = await fetch("http://localhost:3000/api/auth/signin", {
-        method: "POST",
+      const res = await fetch('http://localhost:8080/api/auth/signin', {
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify(formData),
       });
       const data = await res.json();
       if (data.success === false) {
         dispatch(signInFailure(data));
-        setShowError(true);
         return;
       }
-      dispatch(signInSuccess(data));
-      navigate("/");
+      dispatch(signInSuccess(data.user));
+      navigate('/');
     } catch (error) {
       dispatch(signInFailure(error));
     }
   };
+
   return (
-    <div className="z-0 fixed w-full h-full">
-      <Background />
-      <div className="h-full flex justify-center items-center relative z-10">
-        <div className="m-3 z-10 py-5 px-10 md:px-20 md:py-10 shadow-2xl border max-w-2xl rounded-3xl gap-2">
-          <h1 className="min-w-[100px] sm:min-w-[400px] text-3xl text-center font-semibold mb-7 ">
-            Sign In
-          </h1>
-          <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-            <input
-              type="email"
-              placeholder="Email"
-              id="email"
-              className="bg-slate-100 p-3 rounded-lg"
-              onChange={handleChange}
-            />
-            <input
-              type="password"
-              placeholder="Password"
-              id="password"
-              className="bg-slate-100 p-3 rounded-lg"
-              onChange={handleChange}
-            />
-            <button
-              disabled={loading}
-              className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
-            >
-              {loading ? "Loading..." : "Sign In"}
-            </button>
-            <OAuth />
-          </form>
-          <div className="flex gap-2 mt-5">
-            <p>Dont Have an account?</p>
-            <Link to="/sign-up">
-              <span className="text-blue-500">Sign up</span>
-            </Link>
-          </div>
-          <p className="text-red-700 mt-1 text-sm">
-            {error && showError
-              ? error.message || "Something went wrong!"
-              : null}
-          </p>
-        </div>
+    <div className={styles.main_page}>
+      <h1 className={styles.header}>Sign In</h1>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <input
+          type='email'
+          placeholder='Email'
+          id='email'
+          className={styles.input}
+          onChange={handleChange}
+        />
+        <input
+          type='password'
+          placeholder='Password'
+          id='password'
+          className={styles.input}
+          onChange={handleChange}
+        />
+        <button
+          disabled={loading}
+          className={styles.button}
+        >
+          {loading ? 'Loading...' : 'Sign In'}
+        </button>
+        <OAuth />
+      </form>
+      <div className={styles.container}>
+        <p>Dont Have an account?</p>
+        <Link to='/sign-up'>
+          <span className={styles.blue_span}>Sign up</span>
+        </Link>
       </div>
+      <p className={styles.p_show_error}>
+        {error ? error.message || 'Something went wrong!' : ''}
+      </p>
     </div>
   );
 }
