d84acb772211457283c3e9b725e7ad9a0112b7ca
minhphamhk123 <minhphamhklovetory@gmail.com>
2024-01-12 22:34:24 +0700

    12/1/2024

--- client/src/pages/GoogleDocs.jsx
+++ client/src/pages/GoogleDocs.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../styles/GoogleDocs.css'
+import '../styles/GoogleDocs.css';
 
 const GoogleDocsPage = () => {
   return (
@@ -8,6 +8,7 @@ const GoogleDocsPage = () => {
       <iframe
         title="Google Docs Editor"
         src="https://docs.google.com/document/d/1KHexw85b7oB0QY8-hr8Kann_xWhnxXJv/edit#heading=h.gjdgxs"
+        className="google-docs-iframe"
       />
     </div>
   );
